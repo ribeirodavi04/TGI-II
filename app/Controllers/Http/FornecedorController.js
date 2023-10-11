4fc1cdcbb91d30b0b6d5e413ec93ea804a71ebd9
ribeirodavi04 <ribeirodavi04@outlook.com>
2023-10-11 20:14:11 -0300

    Implementação por completo da api na parte de fornecedores

---
+++ app/Controllers/Http/FornecedorController.js
@@ -0,0 +1,52 @@
+const Fornecedor = use('App/Models/Fornecedor');
+
+class FornecedorController {
+    async index({ response }) {
+        const fornecedores = await Fornecedor.all();
+        return response.json(fornecedores);
+    }
+
+    async show({ params, response }) {
+        const fornecedor = await Fornecedor.find(params.id);
+        return response.json(fornecedor);
+    }
+
+    async store({ request, response }) {
+        const data = request.only(['nome', 'idade', 'email', 'telefone', 'cnpj']);
+        const novoFornecedor = new Fornecedor();
+        novoFornecedor.fill(data);
+
+        try {
+            await novoFornecedor.save();
+            return response.status(201).json(novoFornecedor);
+        } catch (error) {
+            return response.status(500).json({ error: 'Erro ao inserir fornecedor.' });
+        }
+    }
+
+    async update({ params, request, response }) {
+        const fornecedor = await Fornecedor.find(params.id);
+        const data = request.only(['nome', 'idade', 'email', 'telefone', 'cnpj']);
+        fornecedor.merge(data);
+
+        try {
+            await fornecedor.save();
+            return response.json(fornecedor);
+        } catch (error) {
+            return response.status(500).json({ error: 'Erro ao atualizar fornecedor.' });
+        }
+    }
+
+    async destroy({ params, response }) {
+        const fornecedor = await Fornecedor.find(params.id);
+
+        try {
+            await fornecedor.delete();
+            return response.status(204).send();
+        } catch (error) {
+            return response.status(500).json({ error: 'Erro ao excluir fornecedor.' });
+        }
+    }
+}
+
+module.exports = FornecedorController;
--- config/database.js
+++ config/database.js
@@ -74,7 +74,7 @@ module.exports = {
       host: Env.get('DB_HOST', 'localhost'),
       port: Env.get('DB_PORT', '5432'),
       user: Env.get('DB_USER', 'postgres'),
-      password: Env.get('DB_PASSWORD', ''),
+      password: Env.get('DB_PASSWORD', 'admin'),
       database: Env.get('DB_DATABASE', 'adonis')
     }
   }
